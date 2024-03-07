b267c6423d03e88f8509e8b444f8c198b4e0a15e
JCYhapon <yhaponjc@gmail.com>
2024-03-07 14:46:40 +0800

    added import file, easier to import the images

---
+++ src/assets/import.js
@@ -0,0 +1,10 @@
+import avatar1 from "./avatar-1.svg";
+import avatar2 from "./avatar-2.svg";
+import avatar3 from "./avatar-3.svg";
+import blog1 from "./blog-1.svg";
+import blog2 from "./blog-2.svg";
+import blog3 from "./blog-3.svg";
+import dotsbg from "./dots-bg.svg";
+import logo from "./logo.svg";
+
+export { avatar1, avatar2, avatar3, blog1, blog2, blog3, dotsbg, logo };

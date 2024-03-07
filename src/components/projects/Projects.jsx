047554b413642f07e08aa77f05ab8ac9bf49762f
JCYhapon <yhaponjc@gmail.com>
2024-03-07 18:38:53 +0800

    removed unused image, and added new for projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./projects.css";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="work container section" id="work">
+      <h2 className="section__title"></h2>
+
+      <div className="work__filters">
+        <span className="work__item"></span>
+        <span className="work__item"></span>
+        <span className="work__item"></span>
+        <span className="work__item"></span>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;

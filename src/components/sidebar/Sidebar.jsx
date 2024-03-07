a7a277cb3d9a6aebde5ed4517f33a1524c6a7a93
JCYhapon <yhaponjc@gmail.com>
2024-03-07 14:59:30 +0800

    structure finished

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./sidebar.css";
+import { logo } from "../../assets/import";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="aside">
+      <a href="#home" className="nav_logo">
+        <img src={logo} alt="" />
+      </a>
+
+      <nav className="nav">
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="icon-home"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="icon-people"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#projects" className="nav__link">
+                <i className="icon-layers"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="icon-bubble"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="nav__footer">
+        <span className="copyright">&copy; 2023 - 2024.</span>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
